1600a4270edcd7655c9fb4925decbbc0fcc3ed02
Alexey Stafeyev <noegon@yandex.ru>
2017-08-18 23:56:00 +0300

    Initial Commit

---
+++ CoreAnimations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreAnimations
+//
+//  Created by Alexey Stafeyev on 18.08.17.
+//  Copyright © 2017 Alexey Stafeyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreAnimations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreAnimations
+//
+//  Created by Alexey Stafeyev on 18.08.17.
+//  Copyright © 2017 Alexey Stafeyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
